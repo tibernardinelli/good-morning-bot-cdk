3592943feefeda7e4c1f13230db4af0ea0c9bf9c
Tiago Bernardinelli <tiago.bernardinelli@gmail.com>
2021-03-13 13:04:57 -0300

    added secret manager

--- app.py
+++ app.py
@@ -1,24 +1,32 @@
 #!/usr/bin/env python3
 
+import os
 from aws_cdk import (
     core as cdk,
     aws_lambda as _lambda,
     aws_lambda_nodejs as _node_lambda,
     aws_events as events,
-    aws_events_targets as targets
-    
+    aws_events_targets as targets,
+    aws_secretsmanager as sm
 )
 
 class cron_fn_stack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        fn = _node_lambda.NodejsFunction(self, "good-morning-fn",
-            entry="../good-morning-bot/index_lambda.js",
-            deps_lock_file_path="../good-morning-bot/package-lock.json"
+        secret = sm.Secret.from_secret_arn(self, "SLACK_TOKEN", 
+            secret_arn=os.getenv('SLACK_TOKEN_ARN')
         )
 
-        events.Rule(self, "cron",
+        fn = _node_lambda.NodejsFunction(self, "good-morning-fn",
+            entry="../good-morning-bot/index_lambda.js",
+            deps_lock_file_path="../good-morning-bot/package-lock.json",
+            environment={
+                "SLACK_TOKEN": secret.secret_value.to_string()
+            }
+        )
+
+        events.Rule(self, "cron-morning",
             schedule=events.Schedule.cron(hour="9", week_day="MON-FRI"),
             targets=[targets.LambdaFunction(handler=fn)]
         )
